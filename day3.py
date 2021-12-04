c17aa2fc6ccc26e32a84a4567046ffd51ce85d96
andybrooker <dy.brooker@gmail.com>
2021-12-04 15:28:53 +0000

    Day 3

--- day3.py
+++ day3.py
@@ -1,5 +1,5 @@
 
-from statistics import mode
+from statistics import mode, multimode
 
 f = open('inputs/input3.txt', 'r')
 data = f.read()
@@ -10,3 +10,33 @@ gamma = ''.join([mode(binstring[i::12]) for i in range(12)])
 epsilon = gamma.translate(str.maketrans("10","01"))
 
 print(int(gamma,2)* int(epsilon,2))
+
+def find_oxygen_rating(arr):
+    i = 0
+    while len(arr) > 1:
+        modal_bit = max(multimode([bit[i] for bit in arr]))
+        print(modal_bit)
+        arr = [bit for bit in arr if bit[i] == modal_bit]
+        print(arr)
+        i += 1
+    
+    return int(''.join(arr), 2)
+
+def find_co2_rating(arr):
+    i = 0
+    while len(arr) > 1:
+        modal_bit = max(multimode([bit[i] for bit in arr]))
+        arr = [bit for bit in arr if bit[i] != modal_bit]
+        i += 1
+    
+    return int(''.join(arr), 2)
+
+
+
+answer1 = find_oxygen_rating(data.splitlines())
+answer2 = find_co2_rating(data.splitlines())
+
+print(answer1 * answer2)
+
+
+
